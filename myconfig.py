f68f2b0e5b5bd194d3a521fecf553df09a18626c
calizer <colonnehome1@gmail.com>
2020-10-29 10:37:15 +1100

    Full working system (servo and motor)

--- myconfig.py
+++ myconfig.py
@@ -50,9 +50,9 @@ CAMERA_HFLIP = False
 DRIVE_TRAIN_TYPE = "L298_SERVO" # SERVO_ESC|DC_STEER_THROTTLE|DC_TWO_WHEEL|SERVO_HBRIDGE_PWM|PIGPIO_PWM|MM1|MOCK
 #
 # L298N
-THROTTLE_IN_PIN1 = 13
-THROTTLE_IN_PIN2 = 11
-THROTTLE_PWM_PIN = 12
+THROTTLE_IN_PIN1 = 8 # ACTUAL PINS 
+THROTTLE_IN_PIN2 = 10
+THROTTLE_PWM_PIN = 32
 
 # #STEERING
 STEERING_CHANNEL = 7            #channel on the 9685 pwm board 0-15
@@ -61,7 +61,7 @@ STEERING_RIGHT_PWM = 200        #pwm value for full right steering
 # 
 #STEERING FOR PIGPIO_PWM
 # STEERING_PWM_PIN = 18           #Pin numbering according to Broadcom numbers
-STEERING_PWM_FREQ = 50          #Frequency for PWM
+# STEERING_PWM_FREQ = 50          #Frequency for PWM
 STEERING_PWM_INVERTED = False   #If PWM needs to be inverted
 # 
 # #THROTTLE
